09aa2ffbaa691571d3ebc86c86322d5e9e1baaba
Caroline Going <carolinegoing2@gmail.com>
2023-10-10 15:21:46 -0400

    Finalized styling for travel experience entries

--- src/components/IndivEntry.js
+++ src/components/IndivEntry.js
@@ -3,20 +3,23 @@ import locationIcon from './img/location-icon.png'
 
 function IndivEntry(props) {
   return (
-    <div>
-        <img src={props.entry.imageUrl} alt=""/>
-        <div ClassName="entry-description">
-            <div ClassName="location-line">
-                <img src={locationIcon} alt="" />
-                <p>{props.entry.location}</p>
-                <a href={props.entry.googleMapsUrl} target="_blank" rel="noopener noreferrer">View on Google Maps</a>
+    <div id="entry-line-container">
+        <div id="entry-container">
+            <img src={props.entry.imageUrl} alt=""/>
+            <div id="entry-description">
+                <div id="location-line">
+                    <img src={locationIcon} alt="location icon" />
+                    <h3>{props.entry.location}</h3>
+                    <a href={props.entry.googleMapsUrl} target="_blank" rel="noopener noreferrer">View on Google Maps</a>
+                </div>
+                <h1>{props.entry.title}</h1>
+                <h5>{props.entry.startDate} - {props.entry.endDate}</h5>
+                <p>
+                    {props.entry.description}
+                </p>
             </div>
-            <h1>{props.entry.title}</h1>
-            <h5>{props.entry.startDate} - {props.entry.endDate}</h5>
-            <p>
-                {props.entry.description}
-            </p>
         </div>
+        <span id="line-separator"></span>
     </div>
   )
 }
--- src/data.js
+++ src/data.js
@@ -1,3 +1,5 @@
+import sydneyOpera from './components/img/sydney-opera-house.png'
+
 export default [
     {
         id: 1,
@@ -7,7 +9,7 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "./img/mount-fuji.png"
+        imageUrl: "./components/img/mount-fuji.png"
     },
     {
         id: 2,
@@ -17,7 +19,7 @@ export default [
         startDate: "27 May, 2021",
         endDate: "8 Jun, 2021",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
-        imageUrl: "./img/sydney-opera-house.png"
+        imageUrl: {sydneyOpera}
     },
     {
         id: 3,
